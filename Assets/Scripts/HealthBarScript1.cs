299c0c85655cb0b66870c6982e77d79d253590c2
Trybusz <74030239+trybusz@users.noreply.github.com>
2022-11-11 19:30:02 -0500

    ability bars

--- Assets/Scripts/HealthBarScript1.cs
+++ Assets/Scripts/HealthBarScript1.cs
@@ -5,10 +5,36 @@ using UnityEngine.UI;
 
 public class HealthBarScript1 : MonoBehaviour
 {
-    public Slider slider;
+    private float primaryValue;
+    private float specialValue;
+    private float primaryValue2;
+    private float specialValue2;
+    public Slider healthSlider;
+    public Slider primarySlider;
+    public Slider specialSlider;
     // Start is called before the first frame update
     public void SetHealth(int maxHealth, int currentHealth)
     {
-        slider.value = (float)currentHealth / (float)maxHealth;
+        healthSlider.value = (float)currentHealth / (float)maxHealth;
+    }
+    public void SetAbilities(float primaryAttackPauseTime, float primaryAttackPause, float specialAttackPauseTime, float specialAttackPause)
+    {
+        primaryValue = primaryAttackPauseTime;
+        specialValue = specialAttackPauseTime;
+        primaryValue2 = primaryAttackPause;
+        specialValue2 = specialAttackPause;
+        
+    }
+    private void Update()
+    {
+        if (primaryValue - Time.timeSinceLevelLoad > 0)
+        {
+            primarySlider.value = (primaryValue - Time.timeSinceLevelLoad) / primaryValue2;
+        }
+        if (specialValue - Time.timeSinceLevelLoad > 0)
+        {
+            specialSlider.value = (specialValue - Time.timeSinceLevelLoad) / specialValue2;
+        }
+
     }
 }
